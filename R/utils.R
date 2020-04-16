e3cf8be888f49da3f462389a3b8615c44dd381ce
Joonas Miettinen <joonas.miettinen@cancer.fi>
2020-04-16 14:42:49 +0300

    get_internal/exported_dataset etc. generalised to any package

--- R/utils.R
+++ R/utils.R
@@ -6,6 +6,9 @@
 #' @title Package Data
 #' @description Utilities to access datasets contained in R packages.
 #' @name package_data_utils
+#' @param package_name `[character]` (mandatory, no default)
+#'
+#' name of R package
 
 
 
@@ -14,16 +17,17 @@
 #' @export
 #' @rdname package_data_utils
 #' @importFrom utils data
-get_exported_dataset_names <- function() {
-  requireNamespace("utils")
-  dataset_names <- utils::data(package = "nordcancore")
+get_exported_dataset_names <- function(package_name) {
+  easyassertions::assert_is_character_nonNA_atom(package_name)
+  dataset_names <- utils::data(package = package_name)
   dataset_names[["results"]][, "Item"]
 }
 #' @export
 #' @rdname package_data_utils
-get_internal_dataset_names <- function() {
+get_internal_dataset_names <- function(package_name) {
+  easyassertions::assert_is_character_nonNA_atom(package_name)
   # exported datasets are not in namespace!
-  ns <- getNamespace("nordcancore")
+  ns <- getNamespace(package_name)
   obj_nms <- names(ns)
   is_df <- vapply(obj_nms, function(obj_nm) is.data.frame(ns[[obj_nm]]),
                   logical(1))
@@ -38,22 +42,20 @@ get_internal_dataset_names <- function() {
 #' @param dataset_name `[character]` (mandatory, no default)
 #'
 #' name of dataset to retrieve.
-get_exported_dataset <- function(dataset_name) {
-  stopifnot(
-    length(dataset_name) == 1,
-    is.character(dataset_name),
-    !is.na(dataset_name)
-  )
+get_exported_dataset <- function(dataset_name, package_name) {
+  easyassertions::assert_is_character_nonNA_atom(dataset_name)
+  easyassertions::assert_is_character_nonNA_atom(package_name)
 
-  expo_data_nms <- get_exported_dataset_names()
+  expo_data_nms <- get_exported_dataset_names(package_name)
   if (!dataset_name %in% expo_data_nms) {
     stop(
       "requested exported dataset ",
       deparse(dataset_name), " is not one of ",
-      deparse(expo_data_nms)
+      deparse(expo_data_nms), " exported datasets in package ",
+      package_name
     )
   }
-  getExportedValue(ns = "nordcancore", name = dataset_name)
+  getExportedValue(ns = package_name, name = dataset_name)
 }
 
 
@@ -64,20 +66,18 @@ get_exported_dataset <- function(dataset_name) {
 #' @rdname package_data_utils
 #' @importFrom data.table is.data.table
 #' @importFrom utils maintainer
-get_internal_dataset <- function(dataset_name) {
-  stopifnot(
-    length(dataset_name) == 1,
-    is.character(dataset_name),
-    !is.na(dataset_name)
-  )
+get_internal_dataset <- function(dataset_name, package_name) {
+  easyassertions::assert_is_character_nonNA_atom(dataset_name)
+  easyassertions::assert_is_character_nonNA_atom(package_name)
 
-  dataset_nms <- get_internal_dataset_names()
+  dataset_nms <- get_internal_dataset_names(package_name)
   if (!dataset_name %in% dataset_nms) {
     stop("Internal error: requested internal dataset ",
-         deparse(dataset_name), " is not one of ",
-         deparse(dataset_nms), ". If you see this, complain to the ",
-         "package maintainer ", utils::maintainer("nordcancore"))
+         deparse(dataset_name), " is not one of the internal datasets ",
+         deparse(dataset_nms), " in package ", package_name,
+         ". If you see this, please complain to the ",
+         "package maintainer ", utils::maintainer(package_name))
   }
 
-  eval(parse(text = paste0("nordcancore:::", dataset_name)))
+  eval(parse(text = paste0(package_name, ":::", dataset_name)))
 }
