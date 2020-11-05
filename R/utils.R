6982a595d256f486aa9a73b9bcfa37daa901abc3
Joonas Miettinen <joonas.miettinen@cancer.fi>
2020-11-05 15:55:21 +0200

    new exported fun handle_subset_arg, copied from basicepistats

--- R/utils.R
+++ R/utils.R
@@ -326,4 +326,129 @@ random_names <- function(
 
 
 
+#' @title Handle Function Subset Argument
+#' @description
+#' This function is intended to be used inside other functions to handle that
+#' function's argument `subset`.
+#' @param subset_arg_nm `[character]` (mandatory, default `"subset"`)
+#'
+#' name of subset argument of the function where this function is used
+#' @param dataset `[environment, data.frame]` (optional, default `emptyenv()`)
+#'
+#' if `subset` is an expression, it can evaluated in this context;
+#' see ?eval
+#' @param function_env `[environment]` (optional, default `parent.frame(1L)`)
+#'
+#' env where to collect `subset` expression from --- the default should work
+#' @param enclosing_env `[environment]` (optional, default `parent.frame(2L)`)
+#'
+#' env where to continue scoping after `dataset`; think of this as the env
+#' where `dataset` exists if it is a data.frame
+#' @param function_call `[call]` (optional, default `sys.call(1L)`)
+#'
+#' in case a warning is emitted due to NA values in the subset, this call
+#' is passed to [base::simpleWarning].
+#'
+#' @details
+#'
+#' The subset arg can be one of `logical`, `integer`, or `NULL`. `NULL` implies
+#' no subset. `logical` must be of length `nrow(dataset)`, if `dataset` is a
+#' data.frame. Similarly, `integer` subset must have values in between
+#' `-nrow(dataset)` and `nrow(dataset)`.
+#'
+#' @examples
+#'
+#' my_fun <- function(x, subset = NULL) {
+#'   stopifnot(is.data.frame(x))
+#'   evaluated_subset <- handle_subset_arg(dataset = x)
+#'   if (is.null(evaluated_subset)) {
+#'     n <- nrow(x)
+#'   } else {
+#'     n <- length(x[[1]][evaluated_subset])
+#'   }
+#'   return(n)
+#' }
+#'
+#' df <- data.frame(a = 1:5, b = 5:1)
+#' my_fun(x = df, subset = df$a > 3)
+#' my_fun(x = df, subset = a > 3)
+#' my_fun(x = df, subset = 1:3)
+#'
+handle_subset_arg <- function(
+  subset_arg_nm = "subset",
+  dataset = emptyenv(),
+  function_env = parent.frame(1L),
+  enclosing_env = parent.frame(2L),
+  function_call = sys.call(1L)
+) {
+  dbc::assert_prod_input_is_character_nonNA_atom(subset_arg_nm)
+  dbc::assert_prod_input_has_one_of_classes(
+    dataset, classes = c("data.frame", "environment")
+  )
+  dbc::assert_prod_input_has_class(
+    function_env, required_class = "environment"
+  )
+  dbc::assert_prod_input_has_class(
+    enclosing_env, required_class = "environment"
+  )
+  stopifnot(
+    is.name(parse(text = subset_arg_nm)[[1L]])
+  )
+
+  # symbol of subset arg of function where this function is used
+  # e.g. my_subset_arg in my_fun(my_subset_arg = column > 1L, dataset = my_data)
+  subset_arg_symbol <- parse(text = subset_arg_nm)[[1L]]
+  # infer the expression supplied to the subset arg of the
+  # function where this function is used; e.g.
+  # e.g. column > 1L in my_fun(my_subset_arg = column > 1L, dataset = my_data)
+  subset_expr <- eval(substitute(
+    substitute(SUBSET_OBJ), list(SUBSET_OBJ = subset_arg_symbol)
+  ), envir = function_env)
+  # evaluate that inferred expression in the context of the dataset and
+  # secondarily the enclosing env
+  value_eval_env <- as.environment(dataset)
+  parent.env(value_eval_env) <- enclosing_env
+  subset_value <- eval(subset_expr, envir = value_eval_env)
+
+  subset_expr_text <- paste0(deparse(subset_expr), collapse = "")
+  dbc::assert_prod_interim_is_one_of(
+    subset_value,
+    x_nm = subset_expr_text,
+    funs = c("report_is_NULL", "report_is_logical_vector",
+             "report_is_number_vector")
+  )
+
+  if (anyNA(subset_value)) {
+    is_na <- is.na(subset_value)
+    msg <- paste0(
+      "there were ", sum(is_na), " NA values passed to argument ",
+      deparse(subset_arg_nm),"; they will not be included in subset"
+    )
+    warning(simpleWarning(msg, call = function_call))
+    if (is.logical(subset_value)) {
+      subset_value[is_na] <- FALSE
+    } else {
+      subset_value <- subset_value[!is_na]
+    }
+  }
+
+  if (is.numeric(subset_value) && any(subset_value %% 1L != 0L)) {
+    stop("numeric subset is not integer-like; e.g. subset = c(1, 5) ",
+         "is fine, but subset = c(1.1, 5.1) is not")
+  }
+
+  if (is.data.frame(dataset) && !is.null(subset_value)) {
+    if (is.logical(subset_value)) {
+      if (!length(subset_value) %in% nrow(dataset)) {
+        stop("dataset has ", nrow(dataset), " rows but logical subset is of ",
+             "length ", length(subset_value))
+      }
+    }
+    if (is.integer(subset_value) && length(subset_value) > 0L && max(subset_value) > nrow(dataset)) {
+      stop("max(", subset_expr_text, ") > number of rows in dataset")
+    }
+  }
+
+  return(subset_value)
+}
 
